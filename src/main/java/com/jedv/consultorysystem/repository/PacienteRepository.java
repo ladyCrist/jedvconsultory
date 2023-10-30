d32f1c23ecc1d924837bdbf23683b8c0d1734f56
ladyCrist <116976078+ladyCrist@users.noreply.github.com>
2023-10-30 13:34:38 -0500

    Delete src directory

--- src/main/java/com/jedv/consultorysystem/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jedv.consultorysystem.repository;
-
-import com.jedv.consultorysystem.entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
-}
